2ba7ed8db5e78a1c169e0607b637e2f7f5ce548d
Wongsakorn Saengsurasak <wong.saengsurasak@gmail.com>
2024-03-10 02:46:54 +0700

    add authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProductModule } from './product/product.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ProductModule]
+  imports: [ProductModule, AuthModule, UsersModule]
 })
 export class AppModule {}
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,17 +1,20 @@
 import { Body, Controller, Delete, Get, HttpException, Param, Post, Put, Query } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductDTO } from './dto/product.dto';
+import { Public } from 'src/auth/decorators/public.decorator';
 
 @Controller('product')
 export class ProductController {
     constructor(private productService: ProductService) { }
 
+    @Public()
     @Get()
     public async getProducts(@Query() query) {
         const { category, minPrice, maxPrice, sort } = query;
         return this.productService.getProducts(category, minPrice, maxPrice, sort);
     }
 
+    @Public()
     @Post()
     public async postProduct(@Body() productDto: Omit<ProductDTO, 'id'>) {
         if (!productDto.name || productDto.price == null || !productDto.category) {
@@ -20,17 +23,19 @@ export class ProductController {
         return this.productService.postProduct(productDto);
     }
 
-
+    @Public()
     @Get(':id')
     public async getProductById(@Param('id') id: number) {
         return this.productService.getProductById(id);
     }
 
+    @Public()
     @Delete(':id')
     public async deleteProductById(@Param('id') id: number) {
         return this.productService.deleteProductById(id);
     }
 
+    @Public()
     @Put(':id')
     public async putProductById(@Param('id') id: number, @Query() query) {
         const propertyName = query.property_name;
